2bf842ae0b0e3cf6c52009cedf482b37c381a344
PC-22 <shahbaziweb@gmail.com>
2023-01-17 15:43:32 +0330

    implement timer context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import { memo } from "react";
-import styled from "styled-components";
+import { createContext, useState } from "react";
 import GlobalStyles from "./globalStyles";
+import { Body } from "./Body";
+import { Header } from "./Header";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme";
+import type { TimerModel } from "./timer.model";
+
+const timerInitialValue = {
+  timeLeft: 5,
+  isStarted: false,
+};
+export const TimerContext = createContext<TimerModel>(timerInitialValue);
 
 const getTheme = () => {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
@@ -10,14 +20,35 @@ const getTheme = () => {
   }
 };
 
-function App() {
+function App(): JSX.Element {
+  const [timer, setTimer] = useState<TimerModel>(timerInitialValue);
+
+  const startTimer = () => {
+    let timeLeft = timerInitialValue.timeLeft;
+    setTimer({
+      isStarted: true,
+      timeLeft: timerInitialValue.timeLeft,
+    });
+    const interval = setInterval(() => {
+      if (timeLeft > 0) {
+        timeLeft--;
+        setTimer(prev => ({ ...prev, timeLeft }));
+      } else {
+        clearInterval(interval);
+        setTimer(prev => ({ ...prev, isStarted: false }));
+      }
+    }, 1000);
+  };
+
   return (
     <ThemeProvider theme={getTheme()}>
       <GlobalStyles />
-      <Title>Welcome</Title>
-      <p>Use this as a starting point to develop your own application :-)</p>
-    </>
+      <TimerContext.Provider value={timer}>
+        <Header visible={false} />
+        <Body onBtnClick={startTimer} />
+      </TimerContext.Provider>
+    </ThemeProvider>
   );
 }
 
-export default memo(App);
+export default App;
