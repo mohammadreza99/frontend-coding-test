96c5b98de66ffac19eb0eae3fce17c9f07aefbf5
PC-22 <shahbaziweb@gmail.com>
2023-01-17 15:43:04 +0330

    create hamburger icon component

---
+++ src/menu.tsx
@@ -0,0 +1,17 @@
+export const MenuIcon = (): JSX.Element => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fill="none"
+      stroke="#d7d7d7"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"
+    />
+  </svg>
+);
